793b425d7904ec49a24d37dc412e644b21b3e60a
Nut Pinyo <bomb.np@gmail.com>
2022-10-28 23:34:03 +0700

    remove addRelatedProblemsHook from Tags collection

--- src/collections/Tags.ts
+++ src/collections/Tags.ts
@@ -1,26 +1,4 @@
-import { CollectionAfterReadHook, CollectionConfig, Where } from 'payload/types'
-
-import { Problem, Tag } from '../payload-types'
-
-// TODO: Reconsider this. A separate API call for problems with query params might be better.
-const addRelatedProblemsHook: CollectionAfterReadHook<Tag> = async ({ doc, req }) => {
-  if (req.payloadAPI === 'REST') {
-    const where: Where = {
-      tags: {
-        equals: doc.id,
-      },
-    }
-    const problems = await req.payload.find<Problem>({
-      collection: 'problems',
-      where: where,
-      depth: 0,
-    })
-    return {
-      ...doc,
-      problems: problems.docs,
-    }
-  }
-}
+import { CollectionConfig } from 'payload/types'
 
 export const Tags: CollectionConfig = {
   slug: 'tags',
@@ -36,7 +14,4 @@ export const Tags: CollectionConfig = {
       type: 'text',
     },
   ],
-  hooks: {
-    afterRead: [addRelatedProblemsHook],
-  },
 }
