abb16588d491ad4964c5fb714ffdd2a528ad6bda
Nut Pinyo <bomb.np@gmail.com>
2023-01-23 00:13:15 +0700

    add students collection

--- src/collections/Students.ts
+++ src/collections/Students.ts
@@ -1,63 +1,156 @@
-import { CollectionConfig } from 'payload/types'
+import { phoneField } from 'payload-plugin-phone-field'
+import { CollectionConfig, Validate } from 'payload/types'
+
+const validateDiscordUsername: Validate<string> = (value) => {
+  if (/^(.+?)#\d{4}$/.test(value)) {
+    return true
+  }
+  return 'Discord username should match the format: username#1234'
+}
 
 export const Students: CollectionConfig = {
   slug: 'students',
   auth: true,
   admin: {
-    useAsTitle: 'email',
+    useAsTitle: 'nickname',
+    group: 'Users',
+    defaultColumns: ['nickname', 'firstName', 'lastName', 'grade', 'email'],
   },
+
   fields: [
     {
-      name: 'nickname',
-      type: 'text'
-    },
-    {
-      name: 'firstName',
-      type: "text",
-    },
-    {
-      name: "lastName",
-      type: "text"
-    },
-    {
-      name: "grade",
-      type: "select",
-      options: [
-        {
-          label: "M4",
-          value: "M4"
-        },
-        {
-          label: "M5",
-          value: "M5"
-        },
-        {
-          label: "M6",
-          value: "M6"
-        },
-      ]
-    },
-    {
-      name: "school",
-      type: "text"
-    },
-    {
-      name: "contact",
-      type: "group",
+      type: 'row',
       fields: [
         {
-          name: "phone",
-          type: "text" // TODO: use phone plugin
+          name: 'nickname',
+          type: 'text',
+          required: true,
         },
         {
-          name: "discord",
-          type: "text" // TODO: validate discord id syntax
+          name: 'gender',
+          type: 'select',
+          required: true,
+          options: [
+            {
+              label: 'Male',
+              value: 'MALE',
+            },
+            {
+              label: 'Female',
+              value: 'FEMALE',
+            },
+            {
+              label: 'Other',
+              value: 'OTHER',
+            },
+            {
+              label: 'Rather not say',
+              value: 'RATHER NOT SAY',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      type: 'row',
+      fields: [
+        {
+          name: 'firstName',
+          type: 'text',
+          required: true,
         },
         {
-          name: "line",
-          type: "text" // TODO: put label as line id
-        }
-      ]
-    }
+          name: 'lastName',
+          type: 'text',
+          required: true,
+        },
+      ],
+    },
+    {
+      type: 'row',
+      fields: [
+        {
+          name: 'grade',
+          type: 'select',
+          required: true,
+          admin: {
+            width: '35%',
+          },
+          options: [
+            {
+              label: 'M4',
+              value: 'M4',
+            },
+            {
+              label: 'M5',
+              value: 'M5',
+            },
+            {
+              label: 'M6',
+              value: 'M6',
+            },
+          ],
+        },
+        {
+          name: 'school',
+          type: 'text',
+          admin: {
+            width: '65%',
+          },
+          required: true,
+        },
+      ],
+    },
+    {
+      name: 'contact',
+      type: 'group',
+      fields: [
+        phoneField(
+          {
+            name: 'phone',
+          },
+          {
+            defaultCountry: 'TH',
+          }
+        ),
+        {
+          type: 'row',
+          fields: [
+            {
+              name: 'discord',
+              type: 'text',
+              admin: {
+                description: 'Username format: username#1234',
+              },
+              validate: validateDiscordUsername,
+            },
+            {
+              name: 'line',
+              type: 'text',
+              label: 'Line ID',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'status',
+      type: 'select',
+      required: true,
+      defaultValue: 'PENDING',
+      admin: {
+        position: 'sidebar',
+      },
+      options: [
+        {
+          label: 'PENDING',
+          value: 'PENDING',
+        },
+        {
+          label: 'APPROVED',
+          value: 'APPROVED',
+        },
+      ],
+    },
   ],
 }
