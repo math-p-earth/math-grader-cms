b7be0c7b46eb93ddcb14f04e42d6617eb4e1d7e8
Nut Pinyo <bomb.np@gmail.com>
2023-09-27 00:55:10 +0700

    feat: add new problem upload editor

--- src/admin/views/ProblemsUpload/components/RenderInput/index.tsx
+++ src/admin/views/ProblemsUpload/components/RenderInput/index.tsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 import { useField } from 'payload/components/forms'
 
-import z from 'zod'
-
-import { problemsUploadSchema } from '../../../../../api/routes/problems/upload/schema'
+import { parseProblemUpload } from '../../../../../api/routes/problems/upload/problem-parser'
 import { ProblemCardList } from '../../../../components/ProblemCardList'
 import './index.scss'
 
@@ -13,111 +11,32 @@ interface RenderInputProps {
 }
 
 const RenderInput: React.FC<RenderInputProps> = ({ inputPath }) => {
-  const { value } = useField<unknown>({
+  const { value: input } = useField<string>({
     path: inputPath,
   })
 
-  if (typeof value === 'undefined') {
+  if (typeof input === 'undefined') {
     return null
   }
 
-  let input: z.infer<typeof problemsUploadSchema>
   try {
-    input = problemsUploadSchema.parse(value)
-  } catch (err) {
-    if (err instanceof z.ZodError) {
-      return (
-        <div>
-          <h3>Invalid input</h3>
-          <ol>
-            {err.issues.map((issue) => (
-              <li key={issue.path.join('.')}>
-                <h4>{issue.path.join('.')}</h4>
-                <p>{issue.message}</p>
-              </li>
-            ))}
-          </ol>
-        </div>
-      )
-    }
+    const parsedProblems = parseProblemUpload(input)
     return (
-      <div>
-        <h3>Invalid input</h3>
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      {input.source ? (
-        <div>
-          <h3 className="success">Source</h3>
-          <p>
-            <strong>Name:</strong> {input.source.name}
-          </p>
-          <p>
-            <strong>Description:</strong> {input.source.description}
-          </p>
-          <p>
-            <strong>Type:</strong> {input.source.type}
-          </p>
-          {input.source.type === 'BOOK' && input.source.book && (
-            <ul>
-              {input.source.book.author && (
-                <li>
-                  <strong>Author:</strong> {input.source.book.author}
-                </li>
-              )}
-              {input.source.book.isbn && (
-                <li>
-                  <strong>ISBN:</strong> {input.source.book.isbn}
-                </li>
-              )}
-            </ul>
-          )}
-          {input.source.type === 'PAPER' && input.source.paper && (
-            <ul>
-              {input.source.paper.timeLimit && (
-                <li>
-                  <strong>Time Limit:</strong> {input.source.paper.timeLimit} minutes
-                </li>
-              )}
-              {input.source.paper.datePublished && (
-                <li>
-                  <strong>Date Published:</strong>{' '}
-                  {new Date(input.source.paper.datePublished).toLocaleDateString()}
-                </li>
-              )}
-            </ul>
-          )}
-        </div>
-      ) : (
-        <h3 className="warning">Source not found</h3>
-      )}
-      {input.problemList ? (
-        <div>
-          <h3 className="success">Problem List</h3>
-          <p>
-            <strong>Name:</strong> {input.problemList.name}
-          </p>
-          <p>
-            <strong>Description:</strong> {input.problemList.description}
-          </p>
-          <p>
-            <strong>Type:</strong> {input.problemList.type}
-          </p>
-        </div>
-      ) : (
-        <h3 className="warning">Problem list not found</h3>
-      )}
       <ProblemCardList
-        problems={input.problems.map((problem) => ({
+        problems={parsedProblems.map((problem) => ({
           ...problem,
           choices: problem.choices?.map((choice) => ({ choice })),
         }))}
       />
-    </div>
-  )
+    )
+  } catch (err) {
+    return (
+      <div>
+        <h3>Invalid input</h3>
+        <p>{err.message}</p>
+      </div>
+    )
+  }
 }
 
 export default RenderInput
--- src/api/routes/problems/upload/index.ts
+++ src/api/routes/problems/upload/index.ts
@@ -1,5 +1,5 @@
 import { Forbidden } from 'payload/errors'
-import { ProblemList, Source } from 'payload/generated-types'
+import { Problem } from 'payload/generated-types'
 import { PayloadRequest } from 'payload/types'
 
 import { Response } from 'express'
@@ -7,10 +7,12 @@ import z from 'zod'
 
 import { AuthUser, isTypeUser } from '../../../../access/type'
 import { withErrorHandler } from '../../../errors/handler/withErrorHandler'
-import { problemsUploadSchema } from './schema'
+import { parseProblemUpload } from './problem-parser'
 
 const zodSchema = z.object({
-  input: problemsUploadSchema,
+  // input: problemsUploadSchema,
+  input: z.string(),
+  source: z.string(),
 })
 
 const canAccess = (user: AuthUser): boolean => {
@@ -24,62 +26,52 @@ async function handler({ body, payload, user }: PayloadRequest, res: Response) {
   if (!canAccess(user)) {
     throw new Forbidden()
   }
-  const { input } = zodSchema.parse(body)
+  const { input, source: sourceId } = zodSchema.parse(body)
+  const parsedProblems = parseProblemUpload(input)
+
+  // vaildate source
+  const source = await payload.findByID({ collection: 'sources', id: sourceId })
+  if (!source) {
+    throw new Error(`Source with id ${sourceId} not found`)
+  }
 
   // create problems
-  // TODO: add support for tags
   const problems = await Promise.all(
-    input.problems.map(async (problemInput) => {
+    parsedProblems.map(async (problem) => {
       return payload.create({
         collection: 'problems',
         data: {
-          type: problemInput.type,
-          content: problemInput.content,
-          choices: problemInput.choices?.map((choice) => ({ choice })),
-          answer: problemInput.answer,
+          type: problem.type,
+          content: problem.content,
+          choices: problem.choices.map((choice) => ({ choice })),
           tags: [],
         },
       })
     })
   )
 
-  // create source
-  let source: Source | null = null
-  if (input.source) {
-    source = await payload.create({
-      collection: 'sources',
-      data: {
-        name: input.source.name,
-        description: input.source.description,
-        type: input.source.type,
-        book: input.source.book,
-        paper: input.source.paper,
-        problems: problems.map((problem) => problem.id),
-      },
-    })
-  }
+  // append new problem ids to list of problems
+  const problemIds = [
+    ...(source.problems?.map((problem: string | Problem) => {
+      if (typeof problem === 'string') {
+        return problem
+      }
+      return problem.id
+    }) ?? []),
+    ...problems.map((problem) => problem.id),
+  ]
 
-  // create problem list
-  let problemList: ProblemList | null = null
-  if (input.problemList) {
-    problemList = await payload.create({
-      collection: 'problem-lists',
-      data: {
-        name: input.problemList.name,
-        description: input.problemList.description,
-        type: input.problemList.type,
-        problems: problems.map((problem) => problem.id),
-      },
-    })
-  }
-  let message = problemList
-    ? `Created problem list "${problemList.name}" with ${problems.length} problems`
-    : `Created ${problems.length} problems`
-  if (source) {
-    message += ` and source "${source.name}"`
-  }
-  message += '.'
-  res.json({ message, problemList, source, problems })
+  // update source
+  await payload.update({
+    collection: 'sources',
+    id: sourceId,
+    data: {
+      problems: problemIds,
+    },
+  })
+
+  const message = `Created ${problems.length} problems and source "${source.name}".`
+  res.json({ message, source, problems })
 }
 
 export default withErrorHandler(handler)
